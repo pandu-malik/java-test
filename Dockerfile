3c0a2d0496268b8aff9830afc1994283dc587892
pandu-malik <muhammad.galang@bfi.co.id>
2021-12-13 14:06:17 +0700

    make it util

--- Dockerfile
+++ Dockerfile
@@ -13,10 +13,10 @@ RUN mkdir conf
 EXPOSE 8002
 
 # The application's jar file
-ARG JAR_FILE=build/libs/pollingpublisher-1.0.1.jar
+ARG JAR_FILE=build/libs/kube-secret-demo-1.0.jar
 
 # Add the application's jar to the container
-ADD ${JAR_FILE} pollingpublisher.jar
+ADD ${JAR_FILE} kube-secret-demo.jar
 
 # Run the jar file
-ENTRYPOINT ["java","-Djava.security.egd=file:/dev/./urandom","-jar","/pollingpublisher.jar"]
+ENTRYPOINT ["java","-Djava.security.egd=file:/dev/./urandom","-jar","/kube-secret-demo.jar"]
--- src/main/java/id/co/bfi/kubesecretdemo/controller/KubeSecret.java
+++ src/main/java/id/co/bfi/kubesecretdemo/controller/KubeSecret.java
@@ -1,35 +1,22 @@
 package id.co.bfi.kubesecretdemo.controller;
 
-import io.kubernetes.client.openapi.ApiClient;
-import io.kubernetes.client.openapi.Configuration;
-import io.kubernetes.client.openapi.apis.CoreV1Api;
-import io.kubernetes.client.openapi.models.V1Secret;
+import id.co.bfi.kubesecretdemo.util.Utility;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class KubeSecret {
-    ApiClient defaultClient = Configuration.getDefaultApiClient();
 
-    CoreV1Api apiInstance = new CoreV1Api();
-    //    Getting value from env variable on docker
+    @Autowired
+    private Utility util;
+
     @Value("${apiVersion}")
     private String configName;
 
-    //    Attemp to getting value via kube API
-    private String kubeString() {
-        defaultClient.setBasePath("http://localhost");
-        String secretName = "secret-test";
-        String nameSpace = "default";
-        try {
-            V1Secret mySecret = apiInstance.readNamespacedSecret(secretName, nameSpace, "true");
-            System.out.println(mySecret);
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-        return "";
-    }
+//    @Value("${secretValue}")
+//    private String secretValue;
 
     @GetMapping("/get-string")
     public String getString() {
@@ -38,6 +25,11 @@ public class KubeSecret {
 
     @GetMapping("/get-string-kube")
     public String getStringKube() {
-        return kubeString();
+        return util.kubeSecretResolver("default", "secret-test");
     }
+
+//    @GetMapping("/get-secret-as-env")
+//    public String getEnvKube() {
+//        return secretValue;
+//    }
 }
